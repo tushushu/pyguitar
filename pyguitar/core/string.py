05cf3fcb701fb8e4ed578db64e1490590d518b1a
tushushu <m18591915960@163.com>
2021-09-04 16:42:24 +0800

    Adapt NoteName class.

--- pyguitar/core/string.py
+++ pyguitar/core/string.py
@@ -3,16 +3,15 @@
 @Author: tushushu
 @Date: 2021-09-02 21:11:03
 """
-from .pitch import Pitch
+from .note_name import NoteName
 
 
 class String:
-    def __init__(self, pitch: Pitch) -> None:
-        self.pitch = pitch
+    def __init__(self, note_name: NoteName) -> None:
+        self._note_name = note_name
 
     def __str__(self) -> str:
-        return f"String with {self.pitch}"
+        return f"{self._note_name.name} string."
 
-    def press_down(self, position: int) -> Pitch:
-        # TODO
-        pass
+    def press_down(self, position: int) -> str:
+        return NoteName.add(self._note_name, position).name
