ddb61f4b5871b84a3970eebf7cd97e946da64f1d
tushushu <m18591915960@163.com>
2021-09-06 20:15:00 +0800

    build

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pyguitar
-A toolkit to learn guitar.
+A toolkit to learn guitar.  
+
+## How to build?
+Run `python setup.py bdist_wheel` to build the project.  
---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: tushushu
+@Date: 2021-09-06 19:52:39
+"""
+from setuptools import setup
+
+
+setup(name='pyguitar',
+      version='0.1.0',
+      description='A toolkit to learn guitar.',
+      author='Tushushu',
+      url='https://github.com/tushushu/pyguitar',
+      packages=['pyguitar'],
+      )
