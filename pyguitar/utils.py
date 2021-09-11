9ca50023914102ed4d585a4ce20f46f58c7c2c7e
tushushu <m18591915960@163.com>
2021-09-11 16:51:36 +0800

    add Color print utils.

--- pyguitar/utils.py
+++ pyguitar/utils.py
@@ -4,9 +4,35 @@
 @Date: 2021-09-11 15:51:41
 """
 import os
+from enum import Enum
 
 
 def speak(text: str, print_text: bool = True) -> None:
     if print_text:
         print(text)
     os.system(f"say '{text}'")
+
+
+class Color(str, Enum):
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+    @classmethod
+    def _print(cls, text: str, color: 'Color') -> None:
+        print(color + text + cls.RESET)
+
+    @classmethod
+    def print_red(cls, text: str) -> None:
+        cls._print(text, cls.RED)
+
+    @classmethod
+    def print_green(cls, text: str) -> None:
+        cls._print(text, cls.GREEN)
--- pyguitar_tasks/fretboard_identification.py
+++ pyguitar_tasks/fretboard_identification.py
@@ -4,10 +4,11 @@
 @Date: 2021-09-06 20:59:02
 """
 import pyguitar as pyg
-from pyguitar.utils import speak
+from pyguitar.utils import speak, Color
 
 
 if __name__ == "__main__":
+    # Initialization.
     speak("Welcome to fretboard identification!")
     speak("Initializing guitar fretboard...")
     fretboard = pyg.Fretboard(
@@ -22,9 +23,15 @@ if __name__ == "__main__":
     )
     speak("The guitar tuning is as below:")
     print(fretboard)
+
+    # Game.
     while True:
         print("Press 'q' to quit the game!")
         inputs = input()
         if inputs == 'q':
             break
+        if inputs == "foo":
+            Color.print_green("Correct!")
+        else:
+            Color.print_red("Wrong!")
     speak("Quiting the game, bye!")
